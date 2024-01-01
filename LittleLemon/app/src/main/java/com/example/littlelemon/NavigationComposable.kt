bda3591c0468d4252062b5555296c32987e4d9f7
Marne Du Toit <mdutoit1536@gmail.com>
2024-01-01 14:16:00 +0200

    Files deleted and added

--- LittleLemon/app/src/main/java/com/example/littlelemon/NavigationComposable.kt
+++ LittleLemon/app/src/main/java/com/example/littlelemon/NavigationComposable.kt
@@ -1,12 +1,10 @@
 import android.content.SharedPreferences
-import android.view.MenuItem
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.littlelemon.Home
 import com.example.littlelemon.HomeScreen
-import com.example.littlelemon.MenuItemNetwork
 import com.example.littlelemon.Onboard
 import com.example.littlelemon.Onboarding
 import com.example.littlelemon.Profile
